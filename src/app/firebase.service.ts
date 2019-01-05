f9efab5851f636d74843b080cb8ee02379b5550a
Sikandar Ali <sikandarali94@hotmail.com>
2019-01-05 16:52:03 +1100

    retrieve title data within home component

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class FirebaseService {
+    dataReturned = new Subject();
+
+    private homeData;
+
     constructor(private http: HttpClient) {}
+
     getHomeData() {
         /* GET data from Firebase to populate in the app. Subscribing here because we want to store the returned data within the service.
          */
         return this.http.get('https://personal-portfolio-f7b43.firebaseio.com/home.json').subscribe(
             result => {
-                console.log(result);
+                this.homeData = result;
+                this.dataReturned.next();
             },
             error => {
                 console.log(error);
             }
         );
+    }
+
+    fetchTitle() {
+        return this.homeData.title;
     }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
+import {Title} from './title.model';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,18 @@ import { FirebaseService } from '../firebase.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  titleData: Title;
 
   constructor(private fb: FirebaseService) { }
 
   ngOnInit() {
     /* Call method in Firebase service that gets the data from the Firebase database.*/
     this.fb.getHomeData();
+    this.fb.dataReturned.subscribe(
+        () => {
+          this.titleData = this.fb.fetchTitle();
+        }
+    );
   }
 
 }
