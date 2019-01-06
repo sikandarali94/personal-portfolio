c923d4ee113c1bd88a47f4a5adc14cc4bad202d9
Sikandar Ali <sikandarali94@hotmail.com>
2019-01-07 06:24:50 +1100

    method to retrieve data for common (contact and footer)

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -6,8 +6,11 @@ import { Subject } from 'rxjs';
 export class FirebaseService {
     dataHomeRetrieved = new Subject(); // Observes if data for the home page has been retrieved successfully.
     dataHomeStored = false; // Indicates if data for the home page has been stored in the service.
+    dataCommonRetrieved = new Subject(); // Observes if data for the common component has been retrieved successfully.
+    dataCommonStored = false; // Indicates if data for the common component has been stored in the service.
 
     private homeData;
+    private commonData; // This data is for contact and footer section which is displayed across all routes.
 
     constructor(private http: HttpClient) {}
 
@@ -18,8 +21,21 @@ export class FirebaseService {
         return this.http.get('https://personal-portfolio-f7b43.firebaseio.com/home.json').subscribe(
             result => {
                 this.homeData = result; // Store the retrieved home page data within the service.
-                this.dataHomeStored = true;
+                this.dataHomeStored = true; // Data for home page has been stored.
                 this.dataHomeRetrieved.next(); // Indicate home page data has been retrieved
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+
+    getCommonData() {
+        return this.http.get('https://personal-portfolio-f7b43.firebaseio.com/common.json').subscribe(
+            result => {
+                this.commonData = result; // Store the retrieved common component data within the service.
+                this.dataCommonStored = true; // Data for common component has been stored.
+                this.dataCommonRetrieved.next();
             },
             error => {
                 console.log(error);
