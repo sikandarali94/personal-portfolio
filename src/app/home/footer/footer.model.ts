5269461877c931573d5d641d8f4ed6ce8f31df5a
Sikandar Ali <sikandarali94@hotmail.com>
2019-01-06 14:48:53 +1100

    defined model for footer section

---
+++ src/app/home/footer/footer.model.ts
@@ -0,0 +1,8 @@
+/* Define model for footer in order to populate the footer section correctly. */
+export class Footer {
+    public heading: string; // A footer should have a heading property of type string.
+
+    constructor(heading: string) {
+        this.heading = heading;
+    }
+}
