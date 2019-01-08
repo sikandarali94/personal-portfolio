490d3d0ef59437edae865689ad3fa5833666eb67
Sikandar Ali <sikandarali94@hotmail.com>
2019-01-09 06:52:37 +1100

    defined model for phases section

---
+++ src/app/project-detail/phases/phase.model.ts
@@ -0,0 +1,12 @@
+/* Define model for phases in order to populate the phases section correctly. */
+export class Phase {
+    public images: string[]; // A phase should have an images property that holds an array of strings.
+    public phase: string; // A phase should have a phase property of string type.
+    public process: string; // A phase should have a process property of string type.
+
+    constructor(images: string[], phase: string, process: string) {
+        this.images = images;
+        this.phase = phase;
+        this.process = process;
+    }
+}
